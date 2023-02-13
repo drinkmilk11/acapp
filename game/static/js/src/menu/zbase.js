7f4b1bf28134d423a6cb572e865813e0f5478bce
luli_luli <ddpcs@qq.com>
2023-02-13 20:37:07 +0800

    complete the game scene

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -7,37 +7,48 @@ class AcGameMenu {
         <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
             单人模式
         </div>
+        <br>
         <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
             多人模式
         </div>
-        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings-mode">
-            设置
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+            退出
         </div>
     </div>
 </div>
-`);        this.root.$ac_game.append(this.$menu);
+`);
+        this.$menu.hide();
         this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
         this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
-        this.$settings_mode = this.$menu.find('.ac-game-menu-field-item-settings-mode');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
 
         this.start();
     }
+
     start() {
         this.add_listening_events();
     }
+
     add_listening_events() {
         let outer = this;
         this.$single_mode.click(function(){
-            outer.hide();   // 关闭主页面
-            outer.root.playground.show();   // 打开游戏界面
+            outer.hide();
+            outer.root.playground.show("single mode");
+        });
+        this.$multi_mode.click(function(){
+            outer.root.playground.show("multi mode");
+        });
+        this.$settings.click(function(){
+            outer.root.settings.logout_on_remote();
         });
     }
 
-    show() {    //显示menu界面
+    show() {  // 显示menu界面
         this.$menu.show();
     }
-    hide() {    //隐藏menu界面
+
+    hide() {  // 关闭menu界面
         this.$menu.hide();
     }
 }
-
